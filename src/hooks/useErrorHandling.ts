0dfd197a241ac89031efd07eb21bf196cc9008dd
James Fang <jamesw.fang@gmail.com>
2024-01-03 11:40:22 -0500

    Update useErrorHandling.ts

--- src/hooks/useErrorHandling.ts
+++ src/hooks/useErrorHandling.ts
@@ -1,17 +1,28 @@
 // hooks/useErrorHandling.ts
-
-/*****************************************************************************************
- * If you're going to try the flag trigger code, you need to replace everything
- * below with the code from 
- * "Handling API Migrations Like a Pro - Immediate Resolution With Flag Triggers", Step 3.
- *****************************************************************************************/
 import { useState } from 'react';
 import { useShoppingCart } from 'use-shopping-cart';
 
 const useErrorHandling = () => {
   const [errorState, setErrorState] = useState(false);
-  
+
   const { clearCart } = useShoppingCart();
+
+  const Trigger = async () => {
+    try {
+      const response = await fetch(
+        'https://app.launchdarkly.com/webhook/triggers/65958d838767121012fbcd14/e6a7cf6d-f167-46b7-abd9-836b1d805711',  
+        {
+          method: "POST",
+          body: JSON.stringify({
+            eventName: "There was an error with the API",
+          }),
+        }
+      );
+      return response.status;
+    } catch (error) {
+      console.log("the fetch did not work");
+    }
+  };
 
   const errorTesting = async () => {
     try {
@@ -23,6 +34,7 @@ const useErrorHandling = () => {
       if (jsonData == "the API is unreachable") {
         setErrorState(true);
          clearCart()
+         Trigger()
         return 502;
       } else {
         setErrorState(false);
